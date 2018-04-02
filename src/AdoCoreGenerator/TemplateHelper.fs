65b9050d92570981eeb1657f8d87548b5fb2f31d
jptarqu <jptarqu@gmail.com>
2018-04-02 16:01:28 -0400

    trying templates

---
+++ src/AdoCoreGenerator/TemplateHelper.fs
@@ -0,0 +1,21 @@
+﻿namespace RequestorsFeedback.Reminders
+
+open System.Collections.Generic
+open System
+open RazorEngine
+open RazorEngine.Templating
+
+type TemplateHelper() =
+    let templates = Dictionary<string, string>()
+    let compiledTemplates = Dictionary<string, bool>()
+
+    member this.LoadTemplates(templatesToLoad: (string* string) seq) =
+        templatesToLoad
+        |> Seq.iter (fun (key, v) -> templates.Add(key, v) )
+
+    member this.Generate(templateKey: string, model: Object) : string =
+        if (compiledTemplates.ContainsKey(templateKey)) then
+            Engine.Razor.Run(name = templateKey, model= model)
+        else 
+            Engine.Razor.RunCompile(templates.[templateKey], templateKey, null, model)
+
